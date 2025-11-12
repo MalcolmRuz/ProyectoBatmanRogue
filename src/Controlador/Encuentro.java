7402fad8933a34fc03dbfacc3f9887d118d4cc81
artor <artor@Oscar>
2025-11-11 23:09:19 -0300

    Version 0.1.2.3

--- src/Controlador/Encuentro.java
+++ src/Controlador/Encuentro.java
@@ -1,4 +1,3 @@
-
 package Controlador;
 
 import Modelo.Villano;
@@ -12,45 +11,42 @@ public class Encuentro {
 
     public Encuentro() {
     }
-    
-   private Villano generarVillano(ProgresionMapa piso) {
-    int nivel = piso.getPisoActual();
-    Villano villanoGenerado = null;
 
-    String sql = "";
+    private Villano generarVillano(ProgresionMapa piso) {
+        int nivel = piso.getPisoActual();
+        Villano villanoGenerado = null;
 
-    if (nivel == 10 || nivel == 20 || nivel == 30) {
-        sql = "SELECT * FROM villano WHERE esJefe = TRUE ORDER BY RAND() LIMIT 1";
-    } else if (nivel <= 9) {
-        sql = "SELECT * FROM villano WHERE nivelDificultad = 1 AND esJefe = FALSE ORDER BY RAND() LIMIT 1"; //<--------------------------------Recordar cambiar base de datos
-    } else if (nivel <= 19) {
-        sql = "SELECT * FROM villano WHERE nivelDificultad = 2 AND esJefe = FALSE ORDER BY RAND() LIMIT 1";
-    } else if (nivel <= 30) {
-        sql = "SELECT * FROM villano WHERE nivelDificultad = 3 AND esJefe = FALSE ORDER BY RAND() LIMIT 1";
-    }
+        String sql = "";
 
-    try (Connection conn = ConexionBD.getConnection();
-         PreparedStatement ps = conn.prepareStatement(sql);
-         ResultSet rs = ps.executeQuery()) {
-
-        if (rs.next()) {
-            villanogenerado = new Villano(
-                rs.getInt("id"),
-                rs.getString("nombre"),
-                rs.getInt("hp"),
-                rs.getInt("ataque"),
-                rs.getInt("defensa"),
-                rs.getString("habilidadEspecial"),
-                rs.getString("dificultad"),
-                rs.getBoolean("esJefe")
-            );
+        if (nivel == 10 || nivel == 20 || nivel == 30) {
+            sql = "SELECT * FROM villano WHERE esJefe = TRUE ORDER BY RAND() LIMIT 1";
+        } else if (nivel <= 9) {
+            sql = "SELECT * FROM villano WHERE nivelDificultad = 1 AND esJefe = FALSE ORDER BY RAND() LIMIT 1"; //<--------------------------------Recordar cambiar base de datos
+        } else if (nivel <= 19) {
+            sql = "SELECT * FROM villano WHERE nivelDificultad = 2 AND esJefe = FALSE ORDER BY RAND() LIMIT 1";
+        } else if (nivel <= 30) {
+            sql = "SELECT * FROM villano WHERE nivelDificultad = 3 AND esJefe = FALSE ORDER BY RAND() LIMIT 1";
         }
 
-    } catch (SQLException e) {
-        e.printStackTrace(); // o maneja el error con logs, pero sin sout
-    }
+        try (Connection conn = ConexionBD.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
 
-    return villanoGenerado;
+            if (rs.next()) {
+                villanogenerado = new Villano(
+                        rs.getInt("id"),
+                        rs.getString("nombre"),
+                        rs.getInt("hp"),
+                        rs.getInt("ataque"),
+                        rs.getInt("defensa"),
+                        rs.getString("habilidadEspecial"),
+                        rs.getString("dificultad"),
+                        rs.getBoolean("esJefe")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // o maneja el error con logs, pero sin sout
+        }
+
+        return villanoGenerado;
+    }
 }
-     
-    
